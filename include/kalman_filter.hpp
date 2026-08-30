8b12c20d761519d96d6666e18b5e824c6de0e2e8
Veeti Pekonen <veeti.pekonen@gmail.com>
2026-08-30 17:22:34 +0300

    Completed kalman filter

--- include/kalman_filter.hpp
+++ include/kalman_filter.hpp
@@ -5,10 +5,17 @@
 
 #include "matrix.h"
 
+enum class [[nodiscard]] KalmanStatus { Ok, DimensionMismatch, NotPosDef, NotInitialized};
+
 class KalmanFilter {
     private:
         mutable Matrix mops;
         int nx;
+        int ny;
+        int nu;
+
+        KalmanStatus status_ = KalmanStatus::NotInitialized; 
+
         matrix A;
         matrix B;
         matrix C;
@@ -18,12 +25,22 @@ class KalmanFilter {
         matrix I;
 
         matrix x_hat;
+
+        KalmanStatus init();
     
     public:
 
-        matrix step(const matrix& z, const matrix& u);
+        // Getters
+        matrix state() const { return x_hat; }
+        matrix covariance() const { return P; }
+        KalmanStatus status() const { return status_; }
+        
+        KalmanStatus predict(const matrix& u);
+        KalmanStatus update(const matrix& z, matrix& y_hat);
+
+        KalmanStatus step(const matrix& z, const matrix& u, matrix& y_hat);
         KalmanFilter(const matrix& A, const matrix& B, const matrix& C,
-                         const matrix& Q, const matrix& R);
+                         const matrix& Q, const matrix& R, const matrix& P, const matrix& x_hat);
 
         ~KalmanFilter() = default;
         
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -4,19 +4,58 @@
 
 
 KalmanFilter::KalmanFilter(const matrix& A, const matrix& B, const matrix& C,
-                           const matrix& Q, const matrix& R) 
-                           : nx(A.rows), A(A), B(B), C(C), Q(Q), R(R),
-                            P(matrix(nx, nx)), I(mops.eye(nx)), x_hat(matrix(nx, 1)) {}
+                           const matrix& Q, const matrix& R, const matrix& P, const matrix& x_hat) 
+                           : nx(A.rows), ny(C.rows), nu(B.columns), A(A), B(B), C(C), Q(Q), R(R),
+                            P(P), I(mops.eye(nx)), x_hat(x_hat) {
+                                status_ = init();
+                            }
 
 
-    matrix KalmanFilter::step(const matrix& z, const matrix& u) {
+    KalmanStatus KalmanFilter::init() {
+        if (A.rows != A.columns || A.rows != B.rows || C.columns != nx ||
+            x_hat.rows != nx || P.rows != P.columns || P.rows != nx ||
+            Q.rows != nx || R.rows != ny || x_hat.columns != 1) {
+            return KalmanStatus::DimensionMismatch;
+        }
+
+        return KalmanStatus::Ok;
+    }
+   
+
+    KalmanStatus KalmanFilter::step(const matrix& z, const matrix& u, matrix& y_hat) {
+        KalmanStatus s = predict(u);
+        if (s != KalmanStatus::Ok) {
+            return s;
+        }
+        return update(z, y_hat);
+    }
+
+    KalmanStatus KalmanFilter::predict(const matrix& u) {
+        if (status_ != KalmanStatus::Ok) {
+            return KalmanStatus::NotInitialized;
+        }
+        if (u.rows != nu) {
+            return KalmanStatus::DimensionMismatch;
+        }
         x_hat = A * x_hat + B * u;
         P = (A * P * A.T() + Q);
+        return KalmanStatus::Ok;
+    }
+
+    KalmanStatus KalmanFilter::update(const matrix& z, matrix& y_hat) {
+        if (status_ != KalmanStatus::Ok) {
+            return KalmanStatus::NotInitialized;
+        }
+        if (z.rows != ny || y_hat.rows != ny || z.columns != 1 ||
+            y_hat.columns != 1) {
+            return KalmanStatus::DimensionMismatch;
+        }
         matrix innovation = z - C * x_hat;
         matrix S = C * P * C.T() + R;
         matrix K = P * C.T() * mops.inverse(S);
         x_hat = x_hat + K * innovation;
-        P = (I - K * C) * P;
-        matrix y_hat = z - C * x_hat;
-        return y_hat;
+        P = (I - K * C) * P; // TODO: make more robust update
+        y_hat = z - C * x_hat;
+        return KalmanStatus::Ok;
+        
     }
